d04cf1b7d49d79d3f82c472d774974bef9ed3b75
Beth Njuguna <libbybetha@gmail.com>
2022-10-02 12:43:31 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsPage = actors.map((actor) => (
+    <div key={actor.name}>
+     {actor.name}<br></br>
+
+      Movies:
+      <ul>
+        {actor.movies.map((movie)=> (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return (
+  <div>
+    {/*{code here}*/}
+    <h1>Actors Page</h1>
+    {actorsPage}
+    </div>
+  );
 }
 
 export default Actors;
